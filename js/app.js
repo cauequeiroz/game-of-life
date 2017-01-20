01f915af117de5741e4e147e2716d41487d5cb3e
Caue Queiroz <cauenqueiroz@gmail.com>
2017-01-20 21:50:23 -0200

    generate board from screen

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ var App = {
 
     $grid: document.querySelector('.grid-container'),
 
+    board: [],
+
+    nextBoard: [],
+
     init: function() {
         App.$grid.addEventListener('click', App.draw, false);
     },
@@ -14,13 +18,62 @@ var App = {
         if ( !App.allowUser || !elem.contains('col') ) return;        
 
         elem.toggle('live');
+    },
+
+    start: function() {
+        App.generateBoard();
+    },
+
+    generateBoard: function() {
+        var grid  = App.$grid,
+            board = [];
+            line  = [];
+
+        for ( var i=0; i<20; i++ ) {
+            for ( var j=0; j<20; j++ ) {
+                var elem = grid.querySelectorAll('.row')[i]
+                               .querySelectorAll('.col')[j];
+
+                line.push(elem.classList.contains('live') ? '1' : '0');
+            }
+
+            board.push(line);
+            line = [];
+        }
+
+        App.board = board;
+        console.table(App.board);
+    },
+
+    generateNextBoard: function() {
+        var board     = App.board,
+            nextBoard = [],
+            line      = [];
+
+        for ( var i=0; i<20; i++ ) {
+            for ( var j=0; j<20; j++ ) {
+                line.push(App.checkNeighbors(i, j) ? '1' : '0');
+            }
+
+            nextBoard.push(line);
+            line = [];
+        }
+
+        App.nextBoard = nextBoard;
+        console.table(App.nextBoard);
+    },
+
+    checkNeighbors: function(row, col) {
+        var board = App.board;
+
+        return board[row][col]==='1' ? false : true;
     }
+
 };
 
 App.init();
 
 /*
-- read dom and create array
 - read array, check each neighboor and generate next array
 - update array with next array
 - read array and update dom
